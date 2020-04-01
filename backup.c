1010d496f4ef4614e9b3ee10f0560e3ec18dd479
theophile <theomolinatti@gmail.com>
2020-04-01 18:46:37 +0200

    New backup

--- backup.c
+++ backup.c
@@ -2,223 +2,202 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
-#include <time.h>
-#include <signal.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <string.h>
 
-//include de la biblio
+//include de la bibliothéque du TD3
 #include "se_fichier.h"
 
-//Fichier configuration
-int config()
+// Création d'un tableau d'entier à partir du fichier de configuration
+// \param[out]	tailleMax	taille du tableau
+// \param[in]	chemin		chemin d'accès au fichier de configuration	
+// \return					un tableau d'entier
+int *config(int *tailleMax, char *chemin)
 {
-	SE_FICHIER fic;
-	char c;
-	char tab[99];
-	int i = 0;
-	
-	fic = SE_ouverture ("lottery.cfg", O_RDONLY);
-
-	if (fic.descripteur == -1)
-		return -1;
-
-	while (SE_lectureCaractere (fic, &c) > 0)
-	{
-		tab[i] = c;
-		printf ("%c", c);
-		i++;
-	}
-	
-	for(int x = 0; x<i; x++)
-	{
-		printf("%c", tab[x]);
-	}
-	
-	printf("%d",i);
-	
-	SE_fermeture (fic);
-	
-	return 0;
-	
-	/*
-	//Tableau 2D
-	int ** tableau2d = malloc(nblignes*sizeof(int));
-
-	for(int i = 0, i < nblignes; ++i)
-
-	{
-		tableau2d[i] = malloc(nbcolonnes*sizeof(int));
-	}
-
-	//apres tableau2d[x][y] pour acces aux elements
-	//la suppression ce fait de la meme facom
-
-	for(int i = 0, i < nblignes; ++i)
-	{
-		free(tableau2d[i]);
-	}
-
-	free(tableau2d);
-	*/
-}
-
-//Lecture du tube
-int serveur(const char *chemin, int argc)
-{
-	SE_FICHIER tube;
 	int i;
 	
-	int tab[argc];
-	
-	tube = SE_ouverture (chemin, O_RDONLY);
+	SE_FICHIER fic = SE_ouverture (chemin, O_RDONLY);
 
-	if (tube.descripteur == -1)
-		return -1;
+	SE_lectureEntier(fic, &i);
 	
-	for(int cmpt = 0; SE_lectureEntier (tube, &i) > 0; cmpt++)
+	int taille = (i*3)+1; // i correspond au nombre de numéro le fois 3 correspond aux numeros gagnants + le nombre de numéros gagnant + les gains 
+	int *tab = malloc (sizeof (int) * taille);
+	
+	tab[0] = i;
+	
+	for (int x = 1; x<taille; x++)
 	{
-		tab[cmpt] = i;
+		SE_lectureEntier(fic, &i);
+		tab[x] = i;
 	}
 	
-	SE_fermeture (tube);
-
-	return 0;
+	SE_fermeture (fic);
+	*tailleMax = taille;
 	
-	
-	//~ SE_FICHIER file;
-	//~ file = SE_ouverture (path, O_RDONLY);
-	/*
-
-		fd = open ("/tmp/tube", O_RDONLY);
-
-		read (fd, buf, BUFFER_SIZE);
-
-		printf ("%s\n", buf);
-
-		close (fd);
- 
-	 //int fd;
-    printf("%d args:\n",argc);
-    for(int i = 2; i < argc; i++) printf("%d ",atoi(argv[i]));
-    printf("\n");
-    */
+	return tab;
 }
 
-//Lecture du tube
-int client(const char *chemin, int argc, char* argv[])
+int testArgClient(int argc, char* argv[])
 {
-	SE_FICHIER tube;
+	for(int x = 2; x < argc; x++)
+	{
+		if(atoi(argv[x]) <= 0)
+		{
+			printf("Erreur d'argument Le client n'a pas était retenue pour \n\n");
+			return 3;
+		}		
+	}
 	
-	tube = SE_ouverture (chemin, O_WRONLY);
+	return 0;
+}
+// ecriture des numéros joué par le client dans le tube
+// \param[in]	chemin		chemin d'acces du tube
+// \param[in]	argc		nombre d'argument 
+// \param[in]	argv		tableau d'argument qui contient les numéros à écrire
+// \return					-1 en cas d'erreur,
+//							0 si aucun problème,
+//							2 si le tube n'existe pas
+int clientEcriture(const char *chemin, int argc, char* argv[])
+{	
+	SE_FICHIER tube = SE_ouverture (chemin, O_WRONLY);
 
 	if (tube.descripteur == -1)
-		return -1;
+		return 2;
+	
+	printf("Client écriture : Les numéros joués sont : ");
 	
 	//On commence à 2 car on ne veux pas lire ./million client
-	for(int i = 2; i<argc; i++)
+	for(int x = 2; x < argc; x++)
 	{
-		if(SE_ecritureEntier(tube, atoi(argv[i])) == -1)
+		printf("%d ", atoi(argv[x]));
+		
+		if(SE_ecritureEntier(tube, atoi(argv[x])) == -1)
+		{
+			printf("Une erreur d'écriture a eu lieu\n");
+			return -1;
+		}
+		
+		if(SE_ecritureCaractere (tube, ' ') == -1)
 		{
 			printf("Une erreur d'écriture a eu lieu\n");
 			return -1;
 		}
 	}
-
-	SE_fermeture (tube);
-
-	return 0;
 	
-	/*
-		fd = open ("/tmp/tube", O_WRONLY);
-
-		strcpy (buf, "Here is Brian!");
-		write (fd, buf, BUFFER_SIZE);
-		close (fd);
-		 
-	*/
+	printf("\n");
+	SE_fermeture (tube);
+	
+	return 0;
 }
 
-int main(int argc, char* argv[]){
-    
-    //doit y avoir plus de trois arguments minimum 
-    /*if (argc < 3)
-    {
-        printf("Syntaxe :\n");
-    }
-    
-    else 
-    {
-        if(strcmp(argv[1], "serveur") == 0) 
-            serveur();
-        
-        else if(strcmp(argv[1], "client") == 0) 
-            client();
-    }
-    */
-    
-   
-    for(int i=2; i<argc; i++)
-    {
-		printf("argument : %s\n", argv[i]);
-	}
-    
-    config();
-    
-    exit(0);
-}
-
-/*
-SE_lectureEntier(cfgFile, &j);
-  // j = 4 
-SE_lectureEntier(cfgFile, &gain[j-1]);
-  // gain[3] = (GAIN POUR 4 CHIFFRES)
-*/
-
-
-//~ if (SE_lectureEntier(cfgFile, &nbrGagnants) == -1)
- //~ PRINT_ON_ERR("Lecture de nbrGagnants")
-
-
-//~ tabGagnants = malloc(nbrGagnants * sizeof(int));
-//~ tabGain     = malloc(nbrGagnants * sizeof(int));
-
-//~ for (int i = 0; i < nbrGagnants; ++i) {
-	//~ if (SE_lectureEntier(cfgFile, &tabGagnants[i]) == -1)
-		 //~ PRINT_ON_ERR("Lecture des numéros gagnants")
-
-
-//~ int fd = open ("/tmp/tube", O_WRONLY);
-    //~ write(fd, &argc, sizeof(int)); // sends count of chosen numbers
-    //~ for(int i = 0; i < argc; i++) write(fd, &argv[i], sizeof(int));//sends chosen numbers
-    //~ close(fd);
-
-//~ write(fd, &entier, sizeof(int));
-
-//~ for(int i = 0; i < size_c; i++){
-        //~ read(fd, &table[i], sizeof(int));
-        //~ printf("%d ",table[i]);
-    //~ }
-    //~ printf("\n");
-
-int serveur(const char *chemin, int *tab, int tailleMax)
-{
-	SE_FICHIER tube;
+// Lecture du résultat envoyé depuis serveur par le tube 
+// \param[in]	chemin		chemin d'acces du tube
+// \return					-1 en cas d'erreur,
+//							 0 le client n'a pas trouvé tout les numéros,
+//							 1 le cient a trouvé tout les numéros
+int clientLecture(const char *chemin)
+{	
 	int i;
-	int numWin;
+	int bonNum;
+	SE_FICHIER tube = SE_ouverture(chemin, O_RDONLY);
+	
+	if (tube.descripteur == -1)
+		return -1;
+	
+	if(SE_lectureEntier(tube, &i) == -1)
+		return -1;
+	
+	bonNum = i;
+	
+	if(i == 0)
+		printf("Vous avez perdu aucun bon numéro\n\n");
+		
+	else
+	{
+		printf("Client lecture : Vous avez %d bon numéro\n", i);
+		
+		if(SE_lectureEntier(tube, &i) == -1)
+			return -1;
+		
+		printf("\t\t Vos gains s'élève à : %d\n\n", i);
+	}
+	
+	if(SE_lectureEntier(tube, &i) == -1)
+			return -1;
+	
+	SE_fermeture (tube);
+	
+	
+	if(bonNum == i)
+		return 1;
+	
+	return 0;
+}
+
+// Gestion des fonction de lecture et d'eciture du tube pour client
+// \param[in]	chemin		chemin d'acces du tube
+// \param[in]	argc		nombre d'argument 
+// \param[in]	argv		tableau d'argument qui contient les numéros à écrire
+// \return					-1 en cas d'erreur,
+//							 0 le client n'a pas trouvé tout les numéros,
+//							 1 le cient a trouvé tout les numéros
+//							 2 si le tube n'existe pas
+int client(const char *chemin, int argc, char* argv[])
+{
+	sleep(1);
+	
+	if(testArgClient(argc, argv) == 3)
+		return 3;
+	
+	switch(clientEcriture(chemin, argc, argv))
+	{
+		case -1 :
+			printf("Une erreur est survenu\n");
+			return -1;
+		
+		case 0 :
+			sleep(1);
+			return clientLecture(chemin);
+			
+		case 2 :
+			printf("There is no ongoing lottery\n");
+			return 2;
+			
+		default :
+			break;
+	}
+	
+	return 0;
+}
+
+// Le seveur créer le tube et lit son contenue 
+// \param[in]	chemin		chemin d'acces du tube
+// \param[in]	tab			tableau avec les informations du fichier .cfg
+// \param[out]	gain		nombre de numéro gagnant
+// \return					-1 en cas d'erreur,
+//							 0 tout c'est bien passé
+int serveurLecture(const char *chemin, int *tab, int *gain)
+{
+	mkfifo ("/tmp/tube", 0600);
+	
+	int i;
+	int numWin = 0;
 	int nbreNum = tab[0];
 	
-	tube = SE_ouverture(chemin, O_RDONLY);
+	SE_FICHIER tube = SE_ouverture(chemin, O_RDONLY);
 
 	if (tube.descripteur == -1)
 		return -1;
 	
+	printf("Serveur lecture : Le joueur a joué les numéros : ");
+	
 	for(int cmpt = 0; cmpt < nbreNum; cmpt++)
 	{
-		SE_lectureEntier(tube, &i);
+		if(SE_lectureEntier(tube, &i) == -1)
+			return -1;
+		
+		printf("%d ", i);
 		
 		for(int x = 1; x <= nbreNum; x++)
 		{
@@ -228,104 +207,148 @@ int serveur(const char *chemin, int *tab, int tailleMax)
 	}
 	
 	SE_fermeture (tube);
-	unlink (chemin);
+	SE_suppression (chemin);
 	
-	// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX //
+	printf("\n\t\t  Le joueur à trouvé %d bon numéro\n", numWin);
+	*gain = numWin;
 	
-	mkfifo ("/tmp/tube", 0600);
-	
-	tube = SE_ouverture(chemin, O_WRONLY);
-
-	if (tube.descripteur == -1)
-		return -1;
-		
-	if(numWin == 0)
-	{
-		if(SE_ecritureEntier(tube, 0) == -1)
-		{
-			printf("Une erreur d'écriture a eu lieu\n");
-			return -1;
-		}
-	}
-	
-	else
-	{
-		for(int x = 0; x<2; x++)
-		{
-			if(SE_ecritureEntier(tube, tab[tailleMax-(2*numWin)+x]) == -1)
-			{
-				printf("Une erreur d'écriture a eu lieu\n");
-				return -1;
-			}
-		}
-	}
-	
-	SE_fermeture (tube);
-		
 	return 0;
 }
 
-int client(const char *chemin, int argc, char* argv[])
+int wrTubeServ(SE_FICHIER tube, int i)
 {
-	SE_FICHIER tube;
-	int i;
-	tube = SE_ouverture (chemin, O_WRONLY);
-
-	if (tube.descripteur == -1)
-		return -1;
-	
-	//On commence à 2 car on ne veux pas lire ./million client
-	for(int x = 2; x<argc; x++)
+	if(SE_ecritureEntier(tube, i) == -1)
 	{
-		if(SE_ecritureEntier(tube, atoi(argv[x])) == -1)
-		{
-			printf("Une erreur d'écriture a eu lieu\n");
-			return -1;
-		}
-	}
-	SE_fermeture (tube);
-	
-	tube = SE_ouverture(chemin, O_RDONLY);
-	
-	if (tube.descripteur == -1)
+		printf("Une erreur d'écriture a eu lieu\n");
 		return -1;
-	
-	SE_lectureEntier(tube, &i);
-	
-	if(i == 0)
-		printf("Vous avez perdu aucun bon numéro\n");
-		
-	else
-	{
-		printf("Vous avez %d bon numéro\n", i);
-		SE_lectureEntier(tube, &i);
-		printf("Vos gains s'élève à : %d\n", i);
 	}
 	
-	SE_fermeture (tube);
-	unlink (chemin);
-		
+	if(SE_ecritureCaractere (tube, ' ') == -1)
+	{
+		printf("Une erreur d'écriture a eu lieu\n");
+		return -1;
+	}
+	
 	return 0;
 }
 
-int main(int argc, char* argv[]){
-	//Comparer le nombre d'argument avec le nombre de numéros de la lottery 
-	int *tab;
-	int tailleMax;
-    
-    tab = config(&tailleMax);
-    
-    mkfifo ("/tmp/tube", 0600);
+// Le seveur créer le tube et lit son contenue 
+// \param[in]	chemin		chemin d'acces du tube
+// \param[in]	tab			tableau avec les informations du fichier .cfg
+// \param[out]	gain		nombre de numéro gagnant
+// \return					-1 en cas d'erreur,
+//							 0 tout c'est bien passé
+int serveurEcriture(const char *chemin, int *tab, int tailleMax ,int gain)
+{
+	printf("Serveur écriture : Transmistions des données vers le client\n");
+	mkfifo (chemin, 0600);
 	
+	SE_FICHIER tube = SE_ouverture(chemin, O_WRONLY);
+
+	if (tube.descripteur == -1)
+		return -1;
+		
+	if(gain == 0)
+	{
+		//~ if(SE_ecritureEntier(tube, 0) == -1)
+		//~ {
+			//~ printf("Une erreur d'écriture a eu lieu\n");
+			//~ return -1;
+		//~ }
+		
+		//~ if(SE_ecritureCaractere (tube, ' ') == -1)
+		//~ {
+			//~ printf("Une erreur d'écriture a eu lieu\n");
+			//~ return -1;
+		//~ }
+		
+		if(wrTubeServ(tube, 0) == -1)
+			return -1;
+	}
+	
+	else
+	{
+		for(int x = 0; x < 2; x++)
+		{
+			//~ if(SE_ecritureEntier(tube, tab[tailleMax-(2*gain)+x]) == -1)
+			//~ {
+				//~ printf("Une erreur d'écriture a eu lieu\n");
+				//~ return -1;
+			//~ }
+			
+			//~ if(SE_ecritureCaractere (tube, ' ') == -1)
+			//~ {
+				//~ printf("Une erreur d'écriture a eu lieu\n");
+				//~ return -1;
+			//~ }
+			
+			if(wrTubeServ(tube, tab[tailleMax-(2*gain)+x]) == -1)
+				return -1;
+		}
+	}
+	
+	
+	//~ if(SE_ecritureEntier(tube, tab[0]) == -1)
+	//~ {
+		//~ printf("Une erreur d'écriture a eu lieu\n");
+		//~ return -1;
+	//~ }
+	
+	//~ if(SE_ecritureCaractere (tube, ' ') == -1)
+	//~ {
+		//~ printf("Une erreur d'écriture a eu lieu\n");
+		//~ return -1;
+	//~ }
+	
+	if(wrTubeServ(tube, tab[0]) == -1)
+		return -1;
+	
+	SE_fermeture(tube);
+	
+	return gain;
+}
+
+// Gestion des fonctions serveurs 
+// \param[in]	chemin		chemin d'acces du tube
+// \param[in]	tab			tableau avec les informations du fichier .cfg
+// \param[in]	tailleMax	nombre de numéro gagnant
+void serveur(const char *chemin, int *tab, int tailleMax)
+{
+	int gain;
+	
+	while(1)
+	{
+		while(1)
+		{	
+			if(serveurLecture(chemin, tab, &gain) != -1)
+				break;
+		}
+	
+		if(serveurEcriture(chemin, tab, tailleMax, gain) == tab[0])
+			break;
+			
+		sleep(1);
+	}
+	
+	SE_suppression (chemin);
+}
+
+int main(int argc, char* argv[])
+{	
 	if(!strcmp(argv[1], "client"))
 	{
-		//Utilisisation de la fonction client
-		client("/tmp/tube", argc, argv);
+		printf("Exécution de client\n");
+		return client("/tmp/tube", argc, argv);
 	}
 	
 	else if(!strcmp(argv[1], "server"))
 	{
-		//Utilisisation de la fonction serveur
+		int *tab;
+		int tailleMax;
+    
+		tab = config(&tailleMax, argv[2]);
+		
+		printf("Exécution de serveur\n");
 		serveur("/tmp/tube", tab, tailleMax);
 		free(tab);
 	}
@@ -333,52 +356,5 @@ int main(int argc, char* argv[]){
 	else
 		printf("L'argument : %s n'est pas reconnue\n", argv[1]);
 	
-    for(int i=2; i<argc; i++)
-    {
-		printf("argument : %s\n", argv[i]);
-	}
-	
     exit(0);
 }
-
-//~ int serveurLectureTest(const char *chemin)
-//~ {
-	//~ int i;
-	//~ int nbreNum = 4;
-	
-	//~ SE_FICHIER tube = SE_ouverture(chemin, O_RDONLY);
-
-	//~ if (tube.descripteur == -1)
-		//~ return -1;
-	
-	//~ printf("Le joueur a joué les numéros : ");
-	
-	//~ for(int cmpt = 0; cmpt < nbreNum; cmpt++)
-	//~ {
-		
-		//~ if(SE_lectureEntier(tube, &i) == -1)
-			//~ return -1;
-
-		//~ printf("%d ", i);
-	//~ }
-	
-	//~ SE_fermeture (tube);
-	//~ SE_suppression (chemin);
-	
-	//~ printf("\nFin de traitement du serveur\n");
-	
-	//~ char str[256];
-	//~ int fd =-1;
-	
-	//~ if ( (fd = open(chemin, O_RDONLY)) == -1)
-	//~ {
-		//~ perror("Erreur open\n");
-		//~ return -1;
-	//~ }
-	
-	//~ read(fd, str, 256);
-	//~ printf("message reçu : %s\n", str);
-	//~ unlink(chemin);
-	
-	//~ return 0;
-//~ }
